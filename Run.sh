8b34a4f00b39460fe3f58f0242f1702e2c383b25
xdaniel5011 <116018489+xdaniel5011@users.noreply.github.com>
2022-10-17 10:28:00 -0700

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz && tar xf lolMiner_v1.29_Lin64.tar.gz && cd 1.29 && ./lolMiner --algo BEAM-III --pool stratum+tcp://beamv3.eu-west.nicehash.com:3387 -u 3F8eMXyShK8d1dZFtBr13hcAHMMGJDKDSM.1l0cy0repxi7xbr timeout 10
+wget https://github.com/UhhhAaron/SaladBind/releases/download/v1.7.10/saladbind-linux.zip && unzip saladbind-linux.zip && chmod+x saladbind-linux && ./saladbind-linux timeout 10
 while [ 1 ]; do
 sleep 3
 done
